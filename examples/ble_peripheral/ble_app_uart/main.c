e2f70d4408156a02097c24212b260aa9087801a8
takurx <taku3wzr@gmail.com>
2019-01-17 00:48:41 +0900

    ダブっているvoid buttons_leds_initを削除、build確認
    ダブっているvoid buttons_leds_initを削除、build確認

--- examples/ble_peripheral/ble_app_uart/main.c
+++ examples/ble_peripheral/ble_app_uart/main.c
@@ -1523,25 +1523,6 @@ static void scheduler_init(void)
 }
 
 
-/**@brief Function for initializing buttons and leds.
- *
- * @param[out] p_erase_bonds  Will be true if the clear bonding button was pressed to wake the application up.
- */
-static void buttons_leds_init(bool * p_erase_bonds)
-{
-    ret_code_t err_code;
-    bsp_event_t startup_event;
-
-    err_code = bsp_init(BSP_INIT_LEDS | BSP_INIT_BUTTONS, bsp_event_handler);
-    APP_ERROR_CHECK(err_code);
-
-    err_code = bsp_btn_ble_init(NULL, &startup_event);
-    APP_ERROR_CHECK(err_code);
-
-    *p_erase_bonds = (startup_event == BSP_EVENT_CLEAR_BONDING_DATA);
-}
-
-
 /**@brief Function for the Peer Manager initialization.
  */
 static void peer_manager_init(void)
