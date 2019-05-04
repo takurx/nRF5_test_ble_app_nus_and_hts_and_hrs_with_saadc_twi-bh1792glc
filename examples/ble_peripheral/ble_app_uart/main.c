956b072f86c7ba5d0485286054d2da9c3bd1a071
takurx <taku3wzr@gmail.com>
2019-05-05 01:13:25 +0900

    関数の整理１５
    関数の整理１５

--- examples/ble_peripheral/ble_app_uart/main.c
+++ examples/ble_peripheral/ble_app_uart/main.c
@@ -1651,6 +1651,41 @@ static void conn_params_init(void)
 
 
 
+/**@brief Function for the Peer Manager initialization.
+ */
+static void peer_manager_init(void)
+{
+    ble_gap_sec_params_t sec_param;
+    ret_code_t           err_code;
+
+    err_code = pm_init();
+    APP_ERROR_CHECK(err_code);
+
+    memset(&sec_param, 0, sizeof(ble_gap_sec_params_t));
+
+    // Security parameters to be used for all security procedures.
+    sec_param.bond           = SEC_PARAM_BOND;
+    sec_param.mitm           = SEC_PARAM_MITM;
+    sec_param.lesc           = SEC_PARAM_LESC;
+    sec_param.keypress       = SEC_PARAM_KEYPRESS;
+    sec_param.io_caps        = SEC_PARAM_IO_CAPABILITIES;
+    sec_param.oob            = SEC_PARAM_OOB;
+    sec_param.min_key_size   = SEC_PARAM_MIN_KEY_SIZE;
+    sec_param.max_key_size   = SEC_PARAM_MAX_KEY_SIZE;
+    sec_param.kdist_own.enc  = 1;
+    sec_param.kdist_own.id   = 1;
+    sec_param.kdist_peer.enc = 1;
+    sec_param.kdist_peer.id  = 1;
+
+    //err_code = pm_sec_params_set(&sec_param);
+    //APP_ERROR_CHECK(err_code);
+
+    //err_code = pm_register(pm_evt_handler);
+    //APP_ERROR_CHECK(err_code);
+}
+
+
+
 /**
  * @brief TWI initialization.
  */
@@ -2049,40 +2084,6 @@ void bsp_event_handler(bsp_event_t event)
         default:
             break;
     }
-}
-
-
-/**@brief Function for the Peer Manager initialization.
- */
-static void peer_manager_init(void)
-{
-    ble_gap_sec_params_t sec_param;
-    ret_code_t           err_code;
-
-    err_code = pm_init();
-    APP_ERROR_CHECK(err_code);
-
-    memset(&sec_param, 0, sizeof(ble_gap_sec_params_t));
-
-    // Security parameters to be used for all security procedures.
-    sec_param.bond           = SEC_PARAM_BOND;
-    sec_param.mitm           = SEC_PARAM_MITM;
-    sec_param.lesc           = SEC_PARAM_LESC;
-    sec_param.keypress       = SEC_PARAM_KEYPRESS;
-    sec_param.io_caps        = SEC_PARAM_IO_CAPABILITIES;
-    sec_param.oob            = SEC_PARAM_OOB;
-    sec_param.min_key_size   = SEC_PARAM_MIN_KEY_SIZE;
-    sec_param.max_key_size   = SEC_PARAM_MAX_KEY_SIZE;
-    sec_param.kdist_own.enc  = 1;
-    sec_param.kdist_own.id   = 1;
-    sec_param.kdist_peer.enc = 1;
-    sec_param.kdist_peer.id  = 1;
-
-    //err_code = pm_sec_params_set(&sec_param);
-    //APP_ERROR_CHECK(err_code);
-
-    //err_code = pm_register(pm_evt_handler);
-    //APP_ERROR_CHECK(err_code);
 }
 
 
