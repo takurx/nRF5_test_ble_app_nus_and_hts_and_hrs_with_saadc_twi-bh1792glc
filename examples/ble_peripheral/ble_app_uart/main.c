61e4e16da59cca45118fedc6ff7db96141907efa
takurx <taku3wzr@gmail.com>
2019-05-05 08:21:40 +0900

    コメントを削除２
    コメントを削除２

--- examples/ble_peripheral/ble_app_uart/main.c
+++ examples/ble_peripheral/ble_app_uart/main.c
@@ -2297,28 +2297,6 @@ static void idle_state_handle(void)
 
 
 
-/**@brief Function for assert macro callback.
- *
- * @details This function will be called in case of an assert in the SoftDevice.
- *
- * @warning This handler is an example only and does not fit a final product. You need to analyse
- *          how your product is supposed to react in case of Assert.
- * @warning On assert from the SoftDevice, the system can only recover on reset.
- *
- * @param[in] line_num    Line number of the failing ASSERT call.
- * @param[in] p_file_name File name of the failing ASSERT call.
- */
-/*
-void assert_nrf_callback(uint16_t line_num, const uint8_t * p_file_name)
-{
-    app_error_handler(DEAD_BEEF, line_num, p_file_name);
-}
-*/
-
-
-
-
-
 /**@brief Application main function.
  */
 int main(void)
