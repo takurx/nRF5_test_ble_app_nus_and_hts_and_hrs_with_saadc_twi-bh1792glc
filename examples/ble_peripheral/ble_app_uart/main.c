13a3528a9cab5c7c09f930daac3b1764de863c60
takurx <taku3wzr@gmail.com>
2019-06-10 02:25:10 +0900

    add fstorage
    add fstorage
    メモリ書き込み位置を
    0x50000にしてみた
    View>Memory Usageで
    Flash(00000000-0007ffff)、.data -0004688b
    まで使ってるっぽいんやけど

--- examples/ble_peripheral/ble_app_uart/main.c
+++ examples/ble_peripheral/ble_app_uart/main.c
@@ -136,6 +136,9 @@
 
 #include "nrf_drv_clock.h"
 
+#include "nrf_fstorage.h"
+#include "nrf_fstorage_sd.h"
+
 #define FIRMWARE_VERSION                "1p0"                                  /* Firmware version, 'ver' command on NUS, :'major'p'minor'*/
 #define DEVICE_NAME                     "Herbio"                               /**< Name of device. Will be included in the advertising data. */
 #define NUS_SERVICE_UUID_TYPE           BLE_UUID_TYPE_VENDOR_BEGIN                  /**< UUID type for the Nordic UART Service (vendor specific). */
@@ -328,7 +331,66 @@ uint8_t twi_tx_buffer[BH1792_TWI_BUFFER_SIZE];
 static void advertising_start(bool erase_bonds);
 static void temperature_measurement_send(void);
 
+static void fstorage_evt_handler(nrf_fstorage_evt_t * p_evt);
 
+NRF_FSTORAGE_DEF(nrf_fstorage_t fstorage) =
+{
+    /* Set a handler for fstorage events. */
+    .evt_handler = fstorage_evt_handler,
+
+    /* These below are the boundaries of the flash space assigned to this instance of fstorage.
+     * You must set these manually, even at runtime, before nrf_fstorage_init() is called.
+     * The function nrf5_flash_end_addr_get() can be used to retrieve the last address on the
+     * last page of flash available to write data. */
+    .start_addr = 0x50000,
+    .end_addr   = 0x6ffff,
+};
+
+/* Dummy data to write to flash. */
+static uint32_t m_data          = 0xBADC0FFE;
+static char     m_hello_world[] = "hello world";
+
+
+
+/**@brief   Helper function to obtain the last address on the last page of the on-chip flash that
+ *          can be used to write user data.
+ */
+static uint32_t nrf5_flash_end_addr_get()
+{
+    uint32_t const bootloader_addr = NRF_UICR->NRFFW[0];
+    uint32_t const page_sz         = NRF_FICR->CODEPAGESIZE;
+    uint32_t const code_sz         = NRF_FICR->CODESIZE;
+
+    return (bootloader_addr != 0xFFFFFFFF ?
+            bootloader_addr : (code_sz * page_sz));
+}
+
+static void fstorage_evt_handler(nrf_fstorage_evt_t * p_evt)
+{
+    if (p_evt->result != NRF_SUCCESS)
+    {
+        NRF_LOG_INFO("--> Event received: ERROR while executing an fstorage operation.");
+        return;
+    }
+
+    switch (p_evt->id)
+    {
+        case NRF_FSTORAGE_EVT_WRITE_RESULT:
+        {
+            NRF_LOG_INFO("--> Event received: wrote %d bytes at address 0x%x.",
+                         p_evt->len, p_evt->addr);
+        } break;
+
+        case NRF_FSTORAGE_EVT_ERASE_RESULT:
+        {
+            NRF_LOG_INFO("--> Event received: erased %d page from address 0x%x.",
+                         p_evt->len, p_evt->addr);
+        } break;
+
+        default:
+            break;
+    }
+}
 
 /**@brief Function for initializing the nrf log module.
  */
@@ -2915,6 +2977,26 @@ static void idle_state_handle(void)
 
 
 
+static void print_flash_info(nrf_fstorage_t * p_fstorage)
+{
+    NRF_LOG_INFO("========| flash info |========");
+    NRF_LOG_INFO("erase unit: \t%d bytes",      p_fstorage->p_flash_info->erase_unit);
+    NRF_LOG_INFO("program unit: \t%d bytes",    p_fstorage->p_flash_info->program_unit);
+    NRF_LOG_INFO("==============================");
+}
+
+void wait_for_flash_ready(nrf_fstorage_t const * p_fstorage)
+{
+    /* While fstorage is busy, sleep and wait for an event. */
+    while (nrf_fstorage_is_busy(p_fstorage))
+    {
+        //power_manage();
+        sd_app_evt_wait();
+    }
+}
+
+
+
 
 
 
@@ -2969,6 +3051,67 @@ int main(void)
     rtc_config();
     NRF_LOG_INFO("finished rtc config.")
 
+    ret_code_t rc;
+    NRF_LOG_INFO("fstorage example started.");
+    nrf_fstorage_api_t * p_fs_api;
+    NRF_LOG_INFO("SoftDevice is present.");
+    NRF_LOG_INFO("Initializing nrf_fstorage_sd implementation...");
+    /* Initialize an fstorage instance using the nrf_fstorage_sd backend.
+     * nrf_fstorage_sd uses the SoftDevice to write to flash. This implementation can safely be
+     * used whenever there is a SoftDevice, regardless of its status (enabled/disabled). */
+    p_fs_api = &nrf_fstorage_sd;
+    rc = nrf_fstorage_init(&fstorage, p_fs_api, NULL);
+    APP_ERROR_CHECK(rc);
+    print_flash_info(&fstorage);
+    /* It is possible to set the start and end addresses of an fstorage instance at runtime.
+     * They can be set multiple times, should it be needed. The helper function below can
+     * be used to determine the last address on the last page of flash memory available to
+     * store data. */
+    (void) nrf5_flash_end_addr_get();
+
+    /* Let's write to flash. */
+    NRF_LOG_INFO("Writing \"%x\" to flash.", m_data);
+    rc = nrf_fstorage_write(&fstorage, 0x50000, &m_data, sizeof(m_data), NULL);
+    APP_ERROR_CHECK(rc);
+
+    wait_for_flash_ready(&fstorage);
+    NRF_LOG_INFO("Done.");
+
+    uint8_t    data[256] = {0};
+    uint32_t i = 0;
+    
+    rc = nrf_fstorage_read(&fstorage, 0x50000, data, 4);
+    if (rc != NRF_SUCCESS)
+    {
+        NRF_LOG_INFO("nrf_fstorage_read() returned: %s\n", nrf_strerror_get(rc));
+    }
+
+    for (i = 0; i < 4; i++)
+    {
+        NRF_LOG_INFO("0x%x ", data[i]);
+    }
+
+/*
+    m_data = 0xDEADBEEF;
+
+    NRF_LOG_INFO("Writing \"%x\" to flash.", m_data);
+    rc = nrf_fstorage_write(&fstorage, 0x3f100, &m_data, sizeof(m_data), NULL);
+    APP_ERROR_CHECK(rc);
+
+    wait_for_flash_ready(&fstorage);
+    NRF_LOG_INFO("Done.");
+
+    rc = nrf_fstorage_read(&fstorage, 0x3f100, data, 4);
+    if (rc != NRF_SUCCESS)
+    {
+        NRF_LOG_INFO("nrf_fstorage_read() returned: %s\n", nrf_strerror_get(rc));
+    }
+
+    for (i = 0; i < 4; i++)
+    {
+        NRF_LOG_INFO("0x%x ", data[i]);
+    }
+*/
     // Start execution.
     printf("\r\nUART started.\r\n");
     NRF_LOG_INFO("Debug logging for UART over RTT started.");
